a2754f0cda793269f93e46129e3f1ab1b0c4151e
c <cui3155@gmail.com>
2023-10-15 23:03:48 +0800

    Create setup-new-machine.sh

---
+++ setup-new-machine.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+npm list -g --depth=0    > npm-g-list.txt
+
+
+##############################################################################################################
+### symlinks to link dotfiles into ~/
+###
+
+#   move git credentials into ~/.gitconfig.local    	http://stackoverflow.com/a/13615531/89484
+#   now .gitconfig can be shared across all machines and only the .local changes
+
+# symlink it up!
+./symlink-setup.sh
+
+# add manual symlink for .ssh/config and probably .config/fish
+
+###
+##############################################################################################################
